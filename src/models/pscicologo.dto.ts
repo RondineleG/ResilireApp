e173cdc828ddac906bf8eeb49e7333aec94e441c
Rondinele Guimaraes <rondineleg@outlook.com>
2021-06-04 01:16:03 -0300

    modeling dtos

---
+++ src/models/pscicologo.dto.ts
@@ -0,0 +1,9 @@
+export interface PacienteDTO {
+    idPsicologo:number,
+    crp: string,
+    email: string,
+    especialidade: string,
+    nome: string,
+    sobrenome: string,
+    telefone: string
+}
